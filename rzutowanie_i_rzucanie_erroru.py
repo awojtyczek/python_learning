c073470b52420f192983054153e8c0a833643e72
awojtyczek <123868749+awojtyczek@users.noreply.github.com>
2023-01-30 21:43:58 +0100

    Update rzutowanie_i_rzucanie_erroru.py

--- rzutowanie_i_rzucanie_erroru.py
+++ rzutowanie_i_rzucanie_erroru.py
@@ -1,10 +1,14 @@
 wiek = input("ile masz lat? ")
-wiek = int(wiek)
+try:
+    wiek = int(wiek)
+except:
+    print("sorry, to nie jest liczba")
+    exit()
+
 if wiek > 30:
     print("lata leca")
 elif wiek <= 0:
     raise ValueError("nie moze byc ujemna")
 else:
     print('mlodosc nie wiecznosc')
-
 
